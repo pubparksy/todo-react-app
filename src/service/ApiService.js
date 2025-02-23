0d4f128edca75e77cece97731a06dee7a2f99ff3
sypark <pubparksy@gmail.com>
2025-02-24 05:03:14 +0900

    fix : Update fetch to get "Error Response" (5.1.4 접근 거부시 로그인 페이지로 라우팅하기)
    1. [FE - ApiService.js] Error Response 받기 위한 코드 변경 (책X, 최신 코드O)
    2. [BE - WebSecurityConfig.java] 인증이 필요 없는 경로에 error 추가 (Spring Boot 2.6.x 이상)

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -12,12 +12,22 @@ export function call(api, method, request) {
     options.body = JSON.stringify(request);
   } 
   
-  return fetch(options.url, options).then((response) => {
-    if (response.status === 200) {
-      return response.json();
-    }
-  }).catch((error) => {
-    console.log("http error");
-    console.log(error);
-  });
+  return fetch(options.url, options)
+    .then((response) =>
+      response.json().then((json) => {
+        if (!response.ok) {
+          // response.ok가 true이면 정상적인 리스폰스를 받은것, 아니면 에러 리스폰스를 받은것.
+          return Promise.reject(json);
+        }
+        return json;
+      })
+    )
+    .catch((error) => {
+      // 추가된 부분
+      if (error.status === 403) {
+        window.location.href = "/login"; // redirect
+      }
+      return Promise.reject(error);
+    });
+
 }
