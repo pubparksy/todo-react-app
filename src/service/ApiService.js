e414b039559203474953e9c4094e9106dcbc5bcc
sypark <pubparksy@gmail.com>
2025-02-27 11:32:00 +0900

    feat : Set AccessToken to LocalStorage, Add AccessToken to API Request header (5.3 로컬 스토리지를 이용한 액세스 토큰 관리)

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,20 @@
 import { API_BASE_URL } from "../api-config";
 
 export function call(api, method, request) {
-  let options = { 
-    headers: new Headers({ "Content-Type": "application/json", }), 
-    url: API_BASE_URL + api, 
-    method: method, 
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  // 로컬 스토리지에서 ACCESS TOKEN 가져오기 
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if (accessToken && accessToken !== null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
+  let options = {
+    headers: headers,
+    url: API_BASE_URL + api,
+    method: method,
   };
 
   if (request) { 
@@ -38,6 +48,8 @@ export function signin(userDTO) {
     .then((response) => {
       // console.log("response : ", response);
       if (response.token) {
+        // 로컬 스토리지에 토큰 저장 
+        localStorage.setItem("ACCESS_TOKEN", response.token);
         // token이 존재하는 경우 Todo 화면으로 리디렉트 
         window.location.href = "/";
       }
