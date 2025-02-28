d41b596e0e67ca825c3feebdee6caf092660280e
sypark <pubparksy@gmail.com>
2025-02-28 13:52:00 +0900

    feat : Add Logout, Loading (5.4 로그아웃과 글리치 해결)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import Todo from './Todo';
 import React, {useState,useEffect} from 'react';
-import { Container, List, Paper } from "@mui/material";
+import { Container, List, Paper, Grid, Button, AppBar, Toolbar, Typography, } from "@mui/material";
 import AddTodo from './AddTodo';
-import { call } from "./service/ApiService";
+import { call, signout } from "./service/ApiService";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // ApiService
   useEffect(() => { 
     call("/todo", "GET", null) 
-    .then((response) => setItems(response.data));
+      .then((response) => {
+        setItems(response.data);
+        setLoading(false);
+      });
   }, []);
 
   const addItem = (item) => { 
@@ -42,14 +46,48 @@ function App() {
       </Paper>
     );
 
-  return (
-    <div className="App">
+  // navigationBar 추가 
+  let navigationBar = (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid justifyContent="space-between" container>
+          <Grid item>
+            <Typography variant="h6">오늘의 할일
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button color="inherit" raised onClick={signout}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
+  // 로딩중이 아닐 때 렌더링할 부분
+  let todoListPage = (
+    <div>
+      {navigationBar} {/* 네비게이션 바 렌더링 */}
       <Container maxWidth="md">
         <AddTodo addItem={addItem}/>
-        <div className='TodoList'>{todoItems}</div>
+        <div className="TodoList">{todoItems}
+        </div>
       </Container>
     </div>
   );
+
+  // 로딩중일 때 렌더링 할 부분
+  let loadingPage = <h1> 로딩중.. </h1>;
+  let content = loadingPage;
+
+  if (!loading) { 
+    /* 로딩중이 아니면 todoListPage를 선택*/
+    content = todoListPage;
+  }
+
+  /* 선택한 content 렌더링 */ 
+  return <div className="App">{content}</div>;
 }
 
 export default App;
